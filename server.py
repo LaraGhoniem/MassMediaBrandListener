82994acfb0bf99327a7b819bbc0376b904ee316f
Mohammed Yasser <mohammedyasserattwa@gmail.com>
2023-02-28 20:45:23 +0200

    update

--- server.py
+++ server.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from Twitter_API import twitter_api
-from youtube import youtube_handler
-from ASR_Model import asr_handler
-from sentiment_analysis_handler import mazajak_api
+from modules.twitter_module.twitter_api import TwitterAPI
+from modules.news_module.news import NewsApi
+from modules.youtube_module.youtube_handler import YouTube
+from modules.asr_module.asr_handler import AsrHandler
+from modules.sentiment_module import mazajak_api
+from modules.database_module.database import Database
 import itertools as IT
+import json
+from bson.objectid import ObjectId
+from classes.user import User
+from classes.listener import ObserverListener
+from classes.engine import Engine
+from pydantic import BaseModel
+#python -m  uvicorn server:app --host 127.0.0.1 --port 80
+
 app = FastAPI()
 
 origins = ["*"]
@@ -16,50 +26,87 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+class JsonData(BaseModel):
+    user_id: str
+    listener_id: str
+    name: str
+    keywords: list
+    category: str
+
+    def toDict(self):
+        return {
+            "user_id": self.user_id,
+            "listener_id": self.listener_id,
+            "name": self.name,
+            "keywords": self.keywords,
+            "category": self.category
+        }
+
+#initialize database connection
+db = Database()
+
+#create Engine
+engine = Engine()
+
+@app.post("/listener/")
+async def main(jsonData: JsonData):
+    data = jsonData.toDict()
+
+    #get user data from dict
+    user_id = data["user_id"]
+    result = [i for i in db.find("users",{"_id":ObjectId(user_id)})][0]
+    user = User(user_id, result["name"],result["email"])
+
+    # create listener
+    listener = ObserverListener(data["listener_id"],data["name"], data["keywords"], data["category"], user)
+    engine.addListener(listener)
+
+    # start engine
+    engine.run()
+    data = engine.listeners[0].result
+    # print(data)
+
+    # update listener in database
+    # db.update("listeners", {"_id":ObjectId(data["listener_id"])}, {"listener_status":"active"})
+    # for i in range(len(engine.listeners)):
+    #     print(engine.listeners[i].result)
+    # return result
+    return data
+    
 
 
-@app.get("/{keyword}")
-async def main(keyword):
-    print(keyword)
-    if keyword != None:
-        # twitterAPI  = twitter_api.TwitterAPI()
-        # query_params = {
-        # 'query': keyword + ' lang:ar',
-        # 'max_results':'10',
-        # 'tweet.fields': 'author_id,in_reply_to_user_id,entities'
-        # }
-        # search_url = "https://api.twitter.com/2/tweets/search/recent"
-        # # json_response = twitterAPI.connect_to_endpoint(search_url,query_params)
-        # # print((json_response["data"][0]["text"]))
-        # counter = 1
-        # # print(json.dumps(json_response, indent=4, sort_keys=True))
-        # # print(coun)
+
+# @app.get("/search/")
+# async def main(keyword: str):
+#     # print(keyword)
+#     if keyword != None or keyword != " ":
+#         twitterAPI  = twitter_api.TwitterAPI(keyword)
+#         myArr = twitterAPI.getResponse("ar")
+#         SpamArray = twitterAPI.spamFilter(myArr)
+#         # print(SpamArray)
+#         twitter_sentimentArray = mazajak_api.predict_list(myArr)
         
-        # myArr = []
-        # while json_response["meta"].get("next_token"):
-        #     query_params["pagination_token"] = json_response["meta"]["next_token"]
-        #     json_response = twitterAPI.connect_to_endpoint(search_url,query_params)
-        #     # print(type(json_response))
-        #     # print((json_response["data"][0]["entities"]))
-        #     myArr += (twitterAPI.preprocessing(json_response["data"],keyword))
-        #     # print(json.dumps(json_response, indent=4, sort_keys=True))
-        #     counter+=1
-            # print(counter)
-            # if(counter == 20):
-            #     break
-        videos = youtube_handler.Youtube(keyword,"ar","EG",2)
-        videos.download("youtube/downloads")
-        asr = asr_handler.AsrHandler(keyword,"YouTube")
-        asr.preprocess()
-        asr.trim()
-        youtube_asr_result = asr.transcribe()
-        # twitter_sentimentArray = mazajak_api.predict_list(myArr)
-        youtube_sentimentArray =  mazajak_api.predict_list(youtube_asr_result)
-        json = []
-        # for text,sentiment in IT.zip_longest(myArr,twitter_sentimentArray):
-        #     json.append({"text":text, "platform": "Twitter", "sentiment":sentiment})
-        for text,sentiment in IT.zip_longest(youtube_asr_result,youtube_sentimentArray):
-            json.append({"text":text, "platform": "YouTube", "sentiment":sentiment})
-        return json
-    # else:
-    #     return ({"error":"keyword is empty"})
+#         articles=ARTICLESAPI(keyword)
+#         art_array,art_titles_array = articles.article_search()
+#         print(len(art_array))
+#         art_sentimentArray = mazajak_api.predict_list(art_array)
+#         # print(len(myArr))
+#         # videos = youtube_handler.Youtube(keyword,"ar","EG",2)
+#         # videos.download("youtube/downloads")
+#         # asr = asr_handler.AsrHandler(keyword,"YouTube")
+#         # asr.preprocess()
+#         # asr.trim()
+#         # youtube_asr_result = asr.transcribe()
+#         # print(myArr)
+        
+#         # youtube_sentimentArray =  mazajak_api.predict_list(youtube_asr_result)
+#         json = []
+#         for text,sentiment,title in IT.zip_longest(art_array,art_sentimentArray,art_titles_array):
+#             json.append({"text":text, "platform": "Articles", "sentiment":sentiment, "title":title})
+#         for text,sentiment,spam in IT.zip_longest(myArr,twitter_sentimentArray,SpamArray):
+#             json.append({"text":text, "platform": "Twitter", "sentiment":sentiment, "spam":str(spam)})
+#         # for text,sentiment in IT.zip_longest(youtube_asr_result,youtube_sentimentArray):
+#         #     json.append({"text":text, "platform": "YouTube", "sentiment":sentiment})
+#         return json
+#     else:
+#         return ({"error":"keyword is empty"})
